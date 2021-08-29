7b5f7bb141837bc6f4f2300107356817360b006c
elizabethkelvin <elizabethkelvin90@gmail.com>
2021-08-29 00:07:11 -0700

    addActivity

--- src/main/java/com/revature/Menu.java
+++ src/main/java/com/revature/Menu.java
@@ -1,15 +1,28 @@
 package com.revature;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.revature.beans.Car;
+import com.revature.beans.Flight;
+import com.revature.beans.Hotel;
+import com.revature.beans.Reservation;
+import com.revature.beans.ReservationStatus;
+import com.revature.beans.ReservationType;
 import com.revature.beans.User;
 import com.revature.beans.UserType;
+import com.revature.beans.Vacation;
 import com.revature.services.UserService;
 
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
 @Component
 public class Menu {
 	@Autowired
@@ -18,11 +31,16 @@ public class Menu {
 	private Scanner scan;
 	
 	private static User loggedUser;
+	
+	//private static Activity activities;
 
 	public static void setLoggedUser(User u) {
 		loggedUser = u;
 	}
-
+	
+	//public static void setActivity(Activity activity) {
+	//	activities = activity;
+	//}
 	public void start() {
 		while(true) {
 			switch(mainMenuInput()) {
@@ -117,7 +135,7 @@ public class Menu {
 					createVacation();
 					break;
 				case "2":
-					editVacationMenu();
+					chooseVacationMenu();
 					break;
 				case "3":
 					doVacationMenu();
@@ -126,7 +144,9 @@ public class Menu {
 					logout();
 					return;
 				case "5":
-					deleteAccount();
+					if(deleteAccount()) {
+						return;
+					}
 					break;
 				case "6":
 					confirmReservationMenu();
@@ -154,17 +174,37 @@ public class Menu {
 		
 	}
 	
-	private void editVacationMenu() {
+	private void chooseVacationMenu() {
+		// COMPLEX
+		// Shows the user all their vacations, 
+		// and lets them choose one to edit
+		Flux<Vacation> vacations = Flux.empty();
+		for (int i = 0; i<loggedUser.getVacations().size();i++) {
+			vacations = Flux.concat(vacations, us.getVacation(loggedUser, loggedUser.getVacations().get(i)));
+		}
+		Flux<Tuple2<Long,Vacation>> vacationsOrdered = vacations.index();
+		vacationsOrdered.subscribe(t -> {
+			System.out.println("Enter "+(t.getT1()+1)+" to edit the vacation to "+t.getT2().getDestination()+" at " +t.getT2().getStartTime());
+		});
+		Long choiceIndex = Long.parseLong(scan.nextLine().trim());
+		Vacation choice = vacationsOrdered
+				.filter(t -> t.getT1().equals(choiceIndex-1))
+				.blockFirst().getT2();
+		editVacationMenu(choice);
+		
+	}
+	
+	private void editVacationMenu(Vacation v) {
 		while(true) {
-			switch(editVacationInput()) {
+			switch(editVacationInput(v)) {
 				case "1":
-					addCar();
+					addCar(v);
 					break;
 				case "2":
-					addHotel();
+					addHotel(v);
 					break;
 				case "3":
-					addFlight();
+					addFlight(v);
 					break;
 				case "4":
 					addActivityMenu();
@@ -179,45 +219,133 @@ public class Menu {
 		
 	}
 	
-
-	private String editVacationInput() {
+	private String editVacationInput(Vacation v) {
 		// Using the scanner, asks the user for one of the above 6 editvacationmenu choices
-		return "";
+		System.out.println("Edit this vacation to "+v.getDestination()+"\n"
+				+ "1: Rent a car\n"
+				+ "2: Reserve a hotel\n"
+				+ "3: Book a flight\n"
+				+ "4: Plan an activity\n"
+				+ "5: Reschedule\n"
+				+ "6: Back");
+		return scan.nextLine().trim();
 	}
+	
+	
+	
 
-	private void addCar() {
-		// Gets user input for which car to add
+	
+
+	private void addCar(Vacation v) {
+		Flux<Car> cars = us.getCars(v.getDestination());
+		
+		Flux<Tuple2<Long,Car>> carsOrdered = cars.index();
+		carsOrdered.subscribe(t -> {
+			System.out.println((t.getT1()+1)+": rent this "+t.getT2().getMake()+" " +t.getT2().getModel()+" for "+t.getT2().getCostPerDay()+" per day");
+		});
+		Long choiceIndex = Long.parseLong(scan.nextLine().trim());
+		Car choice = carsOrdered
+				.filter(t -> t.getT1().equals(choiceIndex-1))
+				.blockFirst().getT2();
+		Reservation r = new Reservation();
+		r.setType(ReservationType.CAR);
+		r.setReservedId(choice.getId());
+		r.setVacationId(v.getId());
+		r.setUsername(loggedUser.getUsername());
+		r.setReservedName(choice.getMake()+" "+choice.getModel());
+		r.setStarttime(v.getStartTime());
+		r.setCost(choice.getCostPerDay());
+		r.setDuration(v.getDuration());
+		r.setStatus(ReservationStatus.AWAITING);
+		us.addReservation(r);
 		
 	}
 
-	private void addHotel() {
-		// Gets user input for which hotel to add
+	private void addHotel(Vacation v) {
+		Flux<Hotel> hotels = us.getHotels(v.getDestination());
 		
+		Flux<Tuple2<Long,Hotel>> hotelsOrdered = hotels.index();
+		hotelsOrdered.subscribe(h -> {
+			System.out.println((h.getT1()+1)+": reserve a room at the "+h.getT2().getName()+" for "+h.getT2().getCostPerNight()+" per night");
+		});
+		Long choiceIndex = Long.parseLong(scan.nextLine().trim());
+		Hotel choice = hotelsOrdered
+				.filter(t -> t.getT1().equals(choiceIndex-1))
+				.blockFirst().getT2();
+		Reservation r = new Reservation();
+		r.setType(ReservationType.HOTEL);
+		r.setReservedId(choice.getId());
+		r.setVacationId(v.getId());
+		r.setUsername(loggedUser.getUsername());
+		r.setReservedName(choice.getName());
+		r.setStarttime(v.getStartTime());
+		r.setCost(choice.getCostPerNight());
+		r.setDuration(v.getDuration());
+		r.setStatus(ReservationStatus.AWAITING);
+		us.addReservation(r);
 	}
 
-	private void addFlight() {
+	private void addFlight(Vacation v) {
 		// Gets user input for which flight to add
+		Flux<Flight> flights = us.getFlights(v.getDestination());
 		
+		Flux<Tuple2<Long,Flight>> flightsOrdered = flights.index();
+		flightsOrdered.subscribe(f -> {
+			System.out.println((f.getT1()+1)+": buy a seat on an "+f.getT2().getAirline()+" flight for "+f.getT2().getTicketPrice());
+		});
+		Long choiceIndex = Long.parseLong(scan.nextLine().trim());
+		Flight choice = flightsOrdered
+				.filter(t -> t.getT1().equals(choiceIndex-1))
+				.blockFirst().getT2();
+		Reservation r = new Reservation();
+		r.setType(ReservationType.FLIGHT);
+		r.setReservedId(choice.getId());
+		r.setVacationId(v.getId());
+		r.setUsername(loggedUser.getUsername());
+		r.setReservedName(choice.getAirline());
+		r.setStarttime(v.getStartTime());
+		r.setCost(choice.getTicketPrice());
+		r.setDuration(v.getDuration());
+		r.setStatus(ReservationStatus.AWAITING);
+		us.addReservation(r);
 	}
 
-	private void addActivityMenu() {
-		// Gets the usres's input for where to search for activities
-		while(true) {
-			String location = scan.nextLine();
-			switch(location) {
-				case "":
-					return;
-				default:
-					addActivity(location);
-			}
+	
+	public void addActivityMenu() {
+		
+		System.out.println("Enter the location to choose your activity. \n");
+		String location = scan.nextLine().trim();	
+		if (addActivity(location) != null) {
+			//System.out.println(" Activity has been added to your vacation. ");		
+		}
+		else {
+			System.out.println(" This location does not have this activity. ");	
+			return;
 		}
 	}
+	
+						
+		
 
-	private void addActivity(String location) {
+	public Boolean addActivity(String location) {
 		// COMPLEX
 		// Shows the user all activities at the given location, 
 		// and lets them choose one to add
-		
+		us.printAllActivitiesbyLocation(location);
+		System.out.println("Select Id of the Activity ");		
+		String Id = scan.nextLine().trim();
+		if (Id == null)
+		{
+			System.out.println("Selection Invalid ");
+			return false;
+		} else {
+		    if (us.addActivity(Id) != null) {
+		    	System.out.println("Activity is added to the Vacation");
+		    	return true;
+		    }
+		    else
+		    	return false;
+		}	
 	}
 
 	private void rescheduleReservationMenu() {
@@ -278,43 +406,21 @@ public class Menu {
 		
 	}
 	
-	private void deleteAccount() {
+	private boolean deleteAccount() {
 		// Asks for the user's confirmation, then deletes their account
-		
+		System.out.println("Are you sure you want to delete your account? (y/n)");
+		if(scan.nextLine().trim().equals("y")) {
+			us.deleteAccount(loggedUser);
+			us.logout();
+			System.out.println("Account Deleted");
+			return true;
+		}
+		return false;
 	}
-
 	private void confirmReservationMenu() {
 		// COMPLEX
 		// Displays all reservations to the staff user, giving them
 		// the option to view each one, then confirm it
 		
 	}
-
-//	1: Register
-//	2: Login
-//		(IF STAFF) Confirm reservations
-//			(Displays all reservations)
-//			Select one to view
-//				Confirm
-//		1: Create Vacation
-//		2: Edit Vacation
-//			(Displays all reservations made)
-//			1: Reserve a car
-//			2: Book a hotel
-//			3: Book a flight
-//			4: Add an activity
-//				Select a location
-//					(Displays all Activities at that location)
-//					Select an activity
-//			5: edit an existing reservation
-//				(Displays all reservations on that vacation)
-//				1: Reschedule the reservation
-//				2: Cancel the reservation
-//		3: Go on Vacation
-//			1: Attend flight
-//			2: Check out of hotel
-//			3: return car
-//		4: Logout
-//		5: Delete account
-//	3: Quit
 }
--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,6 +1,9 @@
 package com.revature.services;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -8,7 +11,12 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.revature.beans.Activity;
+import com.revature.beans.Car;
+import com.revature.beans.Flight;
+import com.revature.beans.Hotel;
+import com.revature.beans.Reservation;
 import com.revature.beans.User;
+import com.revature.beans.Vacation;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,7 +24,7 @@ import reactor.core.publisher.Mono;
 @Service
 public class UserService {
 	private static MultiValueMap<String, String> myCookies = new LinkedMultiValueMap<String, String>();
-
+	String aid = null;
 	
 	public Mono<User> login(User u) {
 		WebClient webClient = WebClient.create();
@@ -58,12 +66,28 @@ public class UserService {
 		activities.subscribe( (act) -> {
 			System.out.println(act);
 		});
-//		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
+	}	
+	
+	public void printAllActivitiesbyLocation(String loc) {
+		WebClient webClient = WebClient.create();
+		Flux<Activity> activities = webClient.get()
+				.uri("http://localhost:8080/activities/" + loc)
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.retrieve()
+				.bodyToFlux(Activity.class);
+		activities.subscribe( (act) -> {
+			System.out.println(act);
+		});
+	}	
+	
+	public Mono<Activity> getActivityById(String id) {
+			WebClient webClient = WebClient.create();
+			Flux <Activity> activities = webClient.get()
+					.uri("http://localhost:8080/activities/" + id)
+					.cookies(cookies -> cookies.addAll(myCookies))
+					.retrieve()
+					.bodyToFlux(Activity.class);
+			return activities.single();
 	}
 
 	public void register(User u) {
@@ -75,5 +99,95 @@ public class UserService {
 				.bodyToMono(User.class)
 				.subscribe(user -> System.out.println(user));
 	}
+	
+	public Mono<Activity> addActivity(String Id) {
+		WebClient webClient = WebClient.create();
+		 
+		Mono<Activity> act = getActivityById(Id);
+
+		return webClient.post()
+				.uri("http://localhost:8080/activities")
+				.body(act, Activity.class)
+				.exchangeToMono(r ->  {
+				for (String key: r.cookies().keySet()) {
+					myCookies.put(key, Arrays.asList(r.cookies().get(key).get(0).getValue()));
+				}
+				return r.bodyToMono(Activity.class);				
+			});
+	}
+	
+	public Flux<Car> getCars(String destination) {
+		WebClient webClient = WebClient.create();
+		String uri = "http://localhost:8080/cars/"+destination;
+		System.out.println(uri);
+		Flux<Car> res = webClient.get()
+				.uri(uri)
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.retrieve()
+				.bodyToFlux(Car.class);
+		res.subscribe(c -> System.out.println(c));
+		return res;
+	}
+	
+	public void addReservation(Reservation r) {
+		WebClient webClient = WebClient.create();
+		webClient.post()
+				.uri("http://localhost:8080/reservations/")
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.body(Mono.just(r),Reservation.class)
+				.retrieve()
+				.bodyToMono(Reservation.class)
+				.subscribe();
+	}
+	
+	public Mono<Vacation> getVacation(User u, UUID id) {
+		WebClient webClient = WebClient.create();
+		String uri = "http://localhost:8080/users/"+u.getUsername()+"/vacations/"+id;
+		Mono<Vacation> res = webClient.get()
+				.uri(uri)
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.exchangeToMono(v -> {
+					return v.bodyToMono(Vacation.class);
+				});
+		return res;
+	}
+	
+	
+	public Flux<Flight> getFlights(String destination) {
+		WebClient webClient = WebClient.create();
+		String uri = "http://localhost:8080/flights/"+destination;
+		System.out.println(uri);
+		Flux<Flight> res = webClient.get()
+				.uri(uri)
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.retrieve()
+				.bodyToFlux(Flight.class);
+		res.subscribe(f -> System.out.println(f));
+		return res;
+	}
+	
+	public Flux<Hotel> getHotels(String destination) {
+		WebClient webClient = WebClient.create();
+		String uri = "http://localhost:8080/hotels/"+destination;
+		System.out.println(uri);
+		Flux<Hotel> res = webClient.get()
+				.uri(uri)
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.retrieve()
+				.bodyToFlux(Hotel.class);
+		res.subscribe(h -> System.out.println(h));
+		return res;
+	}
+	
+	public void deleteAccount(User u) {
+		WebClient webClient = WebClient.create();
+			webClient.delete()
+				.uri("http://localhost:8080/users/"+u.getUsername())
+				.cookies(cookies -> cookies.addAll(myCookies))
+				.retrieve()
+				.toBodilessEntity()
+				.subscribe();
+	}
+
 
 }
